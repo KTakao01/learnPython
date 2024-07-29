105bb7f1723ebd895ddf49411dcd492af235f51e
小泉孝雄 <takoizmi22@gmail.com>
2024-07-29 22:21:42 +0900

    defaultArgs

---
+++ defaultArgs/defaultArgs.py
@@ -0,0 +1,8 @@
+default_message_1 = "Hello" 
+def message(message_1=default_message_1, message_2): 
+    print(f"{message_1} {message_2}") 
+
+default_message_1 = "こんにちは"
+message_2 = "world"
+
+message()

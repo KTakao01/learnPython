97583ee60f1f34a2ec2b89df352411fa0c03aa64
小泉孝雄 <takoizmi22@gmail.com>
2024-07-29 22:30:35 +0900

    add comment

--- defaultArgs/defaultArgs.py
+++ defaultArgs/defaultArgs.py
@@ -1,5 +1,5 @@
 default_message_1 = "Hello" 
-def message(message_1=default_message_1, message_2): 
+def message(message_1=default_message_1, message_2=""): 
     print(f"{message_1} {message_2}") 
 
 default_message_1 = "こんにちは"
